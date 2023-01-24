df28db299b3ba8d778b52b409b77223e2427d57c
mayurpatel8000 <mayurp@dolphinwebsolutions.com>
2023-01-24 14:20:32 +0530

    hook counter 240123

---
+++ src/components/HookCounter.js
@@ -0,0 +1,17 @@
+import React, {useState,useEffect} from 'react'
+
+function HookCounter() {    
+    const [count,setCount] = useState (0)
+    useEffect(() => {
+      document.title = `you clicked ${count} times`
+    })
+  return (
+    <div>
+        <button onClick={() => setCount(count + 1)}>
+            count {count}
+        </button>
+    </div>
+  )
+}
+
+export default HookCounter
